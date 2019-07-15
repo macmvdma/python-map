1aa981639ba3d05e4f8350d3cec02e5a26288ccd
Neil Macneale V <neil.macneale.v@gmail.com>
2019-07-15 13:30:01 -0700

    Added command line args

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import random
 import math
 import sys
 
-map = {}
-
-size = int(input("Map size: "))
-island_size = int(input("Island size: "))
-
-for y in range(size):
-  for x in range(size):
-    map[x, y] = " "
-
 def build(map, x, y):
   recursive_build(map, x, y, x, y, island_size)
 
@@ -38,6 +29,36 @@ def print_map(map):
       print(map[x, y], end="")
     print()
 
-sys.setrecursionlimit(size * size) # python is bad this should be enough to loop through everything, so long as user wont enter a billion
+def new_map(size):
+  map = {}
+  for y in range(size):
+    for x in range(size):
+      map[x, y] = " "
+  return map
+
+def print_usage():
+  print("Usage:  python map.py size [island_size]")
+  print("")
+  print("size:\t\tthe width and height of the map")
+  print("island_size:\tthe average size of the island, and will default to half of size")
+
+def get_args():
+  args = sys.argv
+  args.pop(0)
+  if len(args) < 1:
+    print_usage()
+    exit()
+  elif len(args) < 2:
+    return (int(args[0]), int(int(args[0]) / 2))
+  elif len(args) < 3:
+    return (int(args[0]), int(args[1]))
+  else:
+    print_usage()
+    exit()
+
+size, island_size = get_args()
+
+sys.setrecursionlimit(size * size) # python is bad this should be enough to loop through everything, so long as user doesn't enter a billion
+map = new_map(size)
 build(map, int(size / 2), int(size / 2))
 print_map(map)
